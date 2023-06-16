35f0c4dde88d09b92ba9f0a9e23ea2cafae708a9
Sankaram <das95sankaram@gmail.com>
2023-06-16 18:41:17 +0530

    2nd

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Image from 'next/image'
 import styles from './page.module.css'
+import Button from '@/components/Button/Button'
+import { imageConfigDefault } from 'next/dist/shared/lib/image-config'
 
 const Contact = () => {
   return (
-    <div className={styles.contact}>Contact</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image src='/contact.png' alt='' fill={true} className={styles.image} />
+        </div>
+        <form className={styles.form}>
+          <input type='text' placeholder='name' className={styles.input} />
+          <input type='text' placeholder='email' className={styles.input} />
+          <textarea className={styles.textArea} placeholder='message' cols='30' rows={10}></textarea>
+          <Button url='#' text='Send' />
+        </form>
+      </div>
+    </div>
   )
 }
 
